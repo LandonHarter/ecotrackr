845c290cf30912795f7cc50ccc3672d8e9a795f6
LandonHarter <landonharter@outlook.com>
2023-10-14 16:54:58 -0400

    fix homepage buttons

--- app/(pages)/(header)/(landing)/page.tsx
+++ app/(pages)/(header)/(landing)/page.tsx
@@ -1,8 +1,14 @@
-import styles from "./page.module.scss";
+'use client'
+
+import { useAuthSession } from "@/context/UserContext";
 import ArtBoard from "@/svg/artboard";
 import HeroPic from "@/svg/heropic";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Home() {
+  const { user } = useAuthSession();
+
   return (
     <div className="styles.hero">
       <div className="w-2/3 h-auto absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -17,15 +23,17 @@ export default function Home() {
           <p className="w-5/8 text-gray-400 text-center font-inter text-2xl font-regular leading-8">
             Track emissions and stay up to date on enviromental regulations.
           </p>
-          <div className="flex flex-row gap-[2vw] justify-content items-center mt-[4vh]">
-            <div className="flex p-5 justify-center items-center rounded-3xl bg-green-500">
-              <p className="w-5/8 text-gray-100 text-center font-inter text-2xl font-bold leading-8 m-0 p-0">
-                Start Now
-              </p>
-            </div>
-            <p className="w-5/8 text-gray-400 text-center font-inter text-2xl font-bold leading-8">
+          <div className="flex flex-row gap-2 justify-content items-center mt-[4vh]">
+            <Link href={user ? '/dashboard' : '/signin'}>
+              <Button className="flex p-7 justify-center items-center rounded-xl" color='primary'>
+                <p className="w-5/8 text-gray-100 text-center font-inter text-2xl font-bold leading-8 m-0 p-0">
+                  Start Now
+                </p>
+              </Button>
+            </Link>
+            <Button className="w-5/8 text-gray-400 p-7 text-center font-inter text-2xl font-bold leading-8 bg-transparent">
               Learn More{" "}
-            </p>
+            </Button>
           </div>
         </div>
       </div>
